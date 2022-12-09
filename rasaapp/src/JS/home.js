0da8ab6e8b6b8afab6779be6f2d76443a587494e
Souheil-b <souheil.benali@epitech.eu>
2022-12-09 20:50:55 +0100

    add title, search-bar, search button and display the value of the input

--- rasaapp/src/JS/home.js
+++ rasaapp/src/JS/home.js
@@ -1,15 +1,40 @@
 import React, { useState } from 'react';
 // import { Route } from 'react-router-dom'
+// import { MDBCol, MDBFormInline, MDBBtn } from "mdbreact";
+import '../CSS/home.css'
 
 function Home() {
-    const [count, setCount] = useState(0);
-    
+    const [value, setValue] = useState('');
     return (
-        <div>
-        <p>Count: {count}</p>
-        <button onClick={() => setCount(count + 1)}>Increment</button>
-        </div>
+        <>
+            <div className="scrolling-text">
+                <h1>EPITALKER</h1>
+            </div>
+                <div className="container">
+                    <form className="search-form">
+                        <input className="search-bar" type="text" placeholder="AutoLogin Link..." 
+                        value={value}
+                        onChange={(event) => setValue(event.target.value)}
+                        />
+                        <button
+                            type="submit"
+                            className="search-button"
+                            onClick={() => alert(value)}
+                        >
+                            Rechercher
+                        </button>
+                    </form>
+                </div>
+        </>
     );
 }
+
+
+// const SearchPage = () => {
+//   return (
+
+//   );
+// }
+
 
 export default Home;
